b0699e38e18c3566612e0c661d91900d9b61785a
alvarocalafell <alvarocalafell@gmail.com>
2024-08-01 14:36:57 -0700

    Validation added

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import DataLoader, random_split
 
 from dataset import BilingualDataset, causal_mask
 from model import build_transformer
@@ -19,7 +19,82 @@ from tqdm import tqdm
 from pathlib import Path
 import warnings
 
-# def run_validation(model, validation):
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+    
+    # Precompute the encoder output and reuse it for every token we get from the decoder
+    
+    encoder_output = model.encode(source, source_mask) 
+    #Initialize de decoder with the sos token
+    decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+        # Build mask for the target (decoder_input)
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source).to(device)
+        
+        #Calculate the output of the decoder
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+        
+        # Get the next token, we only want the projection of the next token after the last we gave the encoder
+        prob = model.project(out[:,-1])
+        # Seleect the token with the max probability (because it is a greedy search)
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
+        
+        if next_word == eos_idx:
+            break
+    
+    return decoder_input.squeeze(0) #We squeeze to remove the batch dimension
+        
+    
+    
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_state, writer, num_examples=2):
+    model.eval()
+    count = 0
+    
+    #THIS IS FOR TORCHMETRIC
+    # source_texts = []
+    # expected = []
+    # predicted = []
+    
+    #Size of the control window(just use a default value)
+    console_width = 80
+    
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+            
+            assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
+            
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+            
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            #THIS IS FOR TORCHMETRIC
+            # source_texts.append(source_text)
+            # expected.append(target_text)
+            # predicted.append(model_out_text)
+            
+            #Print to the console
+            print_msg('-'*console_width)
+            print_msg(f'SOURCE:{source_text}')
+            print_msg(f'TARGET:{target_text}')
+            print_msg(f'PREDICTED:{model_out_text}')
+            
+            if count == num_examples:
+                break
+    #TODO
+    #if writer:
+        #TorchMetric CharErrorRate, BLEU, WordErrorRate
+    
+    
     
 
 def get_or_build_tokenizer(config, ds, lang):
@@ -107,10 +182,12 @@ def train_model(config):
     loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1 ).to(device)  #Here we use lable smoothing, makes model less confident about its decision 
                                                                                         #takes a small percentage of the probability of the word chosen and distributes it around the other tokens, improves accuracy and reduces overfitting
     for epoch in range(initial_epoch, config['num_epochs']):
+        
+        torch.cuda.empty_cache()
         model.train()
         batch_iterator = tqdm(train_dataloader, desc = f'Processing epoch {epoch:02d} ')
         for batch in batch_iterator:
-            
+
             encoder_input = batch['encoder_input'].to(device) # (B, seq_len)
             decoder_input = batch['decoder_input'].to(device) # (B, seq_len)
             encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len) Here we only hide the padding
@@ -139,8 +216,11 @@ def train_model(config):
             optimizer.step()
             optimizer.zero_grad()
             
+            
             global_step += 1
             
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+
         # Save the model at the end of every epoch
         model_filename = get_weight_file_path(config, f'{epoch:02d}')
         
